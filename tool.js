75d37d090a2229d1a25f777f2ddb84d07b2f1181
Victor Sheyanov <victor.sheyanov@gmail.com>
2022-05-27 16:20:56 +0200

    removed q dependency, testing cleanup

--- tool.js
+++ tool.js
@@ -1,9 +1,5 @@
-var fs = require('fs');
-var path = require('path');
-var crypto = require('crypto');
 var gutil = require('gulp-util');
 var AWS = require('aws-sdk');
-var Q = require('q');
 
 module.exports = function(options) {
   var cloudfront = new AWS.CloudFront({
@@ -13,56 +9,48 @@ module.exports = function(options) {
   });
 
   var updateDefaultRootObject = function (defaultRootObject) {
+    return new Promise((resolve, reject) => {
+      // Get the existing distribution id
+      cloudfront.getDistribution({ Id: options.distributionId }, function(err, data) {
+        if (err) {
+          reject(err);
+        } else {
 
-    var deferred = Q.defer();
+          //   // AWS Service returns errors if we don't fix these
+          //   if (data.DistributionConfig.Comment === null) data.DistributionConfig.Comment = '';
+          //   if (data.DistributionConfig.Logging.Enabled === false) {
+          //     data.DistributionConfig.Logging.Bucket = '';
+          //     data.DistributionConfig.Logging.Prefix = '';
+          //   }
 
-    // Get the existing distribution id
-    cloudfront.getDistribution({ Id: options.distributionId }, function(err, data) {
+          //   // Causing problems on a default cloudfront setup, why is this needed?
+          //   if (data.DistributionConfig.Origins.Items instanceof Array && data.DistributionConfig.Origins.Items[0].S3OriginConfig.OriginAccessIdentity === null) {
+          //     data.DistributionConfig.Origins.Items[0].S3OriginConfig.OriginAccessIdentity = '';
+          //   }
 
-      if (err) {
-        deferred.reject(err);
-      } else {
-
-        // AWS Service returns errors if we don't fix these
-        if (data.DistributionConfig.Comment === null) data.DistributionConfig.Comment = '';
-        if (data.DistributionConfig.Logging.Enabled === false) {
-          data.DistributionConfig.Logging.Bucket = '';
-          data.DistributionConfig.Logging.Prefix = '';
-        }
-
-        // Causing problems on a default cloudfront setup, why is this needed?
-        if (data.DistributionConfig.Origins.Items instanceof Array && data.DistributionConfig.Origins.Items[0].S3OriginConfig.OriginAccessIdentity === null) {
-          data.DistributionConfig.Origins.Items[0].S3OriginConfig.OriginAccessIdentity = '';
-        }
-
-        if (data.DistributionConfig.DefaultRootObject === defaultRootObject.substr(1)) {
-          gutil.log('gulp-cloudfront:', "DefaultRootObject hasn't changed, not updating.");
-          return deferred.resolve();
-        }
-
-        // Update the distribution with the new default root object (trim the precedeing slash)
-        data.DistributionConfig.DefaultRootObject = defaultRootObject.substr(1);
-
-        cloudfront.updateDistribution({
-          IfMatch: data.ETag,
-          Id: options.distributionId,
-          DistributionConfig: data.DistributionConfig
-        }, function(err, data) {
-
-          if (err) {
-            deferred.reject(err);
-          } else {
-            gutil.log('gulp-cloudfront:', 'DefaultRootObject updated to [' + defaultRootObject.substr(1) + '].');
-            deferred.resolve();
+          if (data.DistributionConfig.DefaultRootObject === defaultRootObject.substr(1)) {
+            gutil.log('gulp-cloudfront:', "DefaultRootObject hasn't changed, not updating.");
+            return resolve();
           }
 
-        });
+          // Update the distribution with the new default root object (trim the precedeing slash)
+          data.DistributionConfig.DefaultRootObject = defaultRootObject.substr(1);
 
-      }
+          cloudfront.updateDistribution({
+            IfMatch: data.ETag,
+            Id: options.distributionId,
+            DistributionConfig: data.DistributionConfig
+          }, function(err, data) {
+            if (err) {
+              reject(err);
+            } else {
+              gutil.log('gulp-cloudfront:', 'DefaultRootObject updated to [' + defaultRootObject.substr(1) + '].');
+              resolve();
+            }
+          });
+        }
+      });
     });
-
-    return deferred.promise;
-
   };
 
   return {
